b7c4c17a17d60c518adaf5f76c816450179d6b40
Thomas Holmes <thomasholmes@outlook.com>
2015-03-16 23:11:58 +0000

    First commit. Controller can stage a vessel and not much else.

---
+++ JebNet/ControllerPartModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JebNet
+{
+    public class ControllerPartModule : PartModule
+    {
+        /// <summary>
+        /// Called when the part is started by Unity.
+        /// </summary>
+        public override void OnStart(StartState state)
+        {
+            print("part module started");
+        }
+
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+
+        }
+
+    }
+}
