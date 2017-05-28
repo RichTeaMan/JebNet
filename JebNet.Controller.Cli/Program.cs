932817c9cb2ef6c0815ef5bafc5f67090e37bf3b
Thomas Holmes <thomasholmes@outlook.com>
2017-05-28 13:42:44 +0100

    Added utility project.

--- JebNet.Controller.Cli/Program.cs
+++ JebNet.Controller.Cli/Program.cs
@@ -1,15 +1,19 @@
 ﻿using JebNet.Controller.Domain;
 using JebNet.Controller.Domain.Service;
+using JetNet.Controller.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace JebNet.Controller.Cli
 {
     class Program
     {
+        private const int WAIT_PERIOD = 100;
+
         private static ControllerService controllerService = new ControllerService();
 
         static void Main(string[] args)
@@ -17,16 +21,41 @@ namespace JebNet.Controller.Cli
             Console.WriteLine("Press enter to launch vehicle.");
             Console.ReadLine();
 
+            PidController throttleController = new PidController()
+            {
+                Kp = 0.004,
+                Ki = 0.000,
+                Kd = 0.2,
+                MinOutput = -1.0,
+                MaxOutput = 1.0
+            };
+
             Vessel vessel = controllerService.FetchVesselStatus();
+            double throttle = 0;
 
-            var controlState = new ControlState(vessel.ControlState);
-            controlState.MainThrottle = 0.3f;
-            controlState.StageVessel();
-            
-            controllerService.SendVessel(controlState);
+            {
+                var controlState = new ControlState(vessel.ControlState);
+                controlState.StageVessel();
+                controlState.MainThrottle = (float)throttle;
+                vessel = controllerService.SendVessel(controlState);
+            }
+            while (true)
+            {
 
-            Console.WriteLine("Vehicle launched.");
-            Console.ReadLine();
+                throttle += throttleController.control(1000, vessel.Altitude, WAIT_PERIOD);
+                throttle = Math.Max(0.0, throttle);
+                throttle = Math.Min(1.0, throttle);
+
+                var loopControlState = new ControlState(vessel.ControlState);
+                loopControlState.MainThrottle = (float)throttle;
+
+                Console.WriteLine("Throttle at {0}%.", throttle * 100);
+
+                vessel = controllerService.SendVessel(loopControlState);
+
+                Thread.Sleep(WAIT_PERIOD);
+            }
+
         }
     }
 }
---
+++ JetNet.Controller.Utilities/PidController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JetNet.Controller.Utilities
+{
+    public class PidController
+    {
+
+
+        /// <summary>
+        /// Previous error.
+        /// </summary>
+        public double PreviousError { get; private set; } = 0;
+
+        /// <summary>
+        /// Proportional gain.
+        /// </summary>
+        public double Kp { get; set; }
+
+        /// <summary>
+        /// Integral gain.
+        /// </summary>
+        public double Ki { get; set; }
+
+        /// <summary>
+        /// Derivative gain.
+        /// </summary>
+        public double Kd { get; set; }
+
+        /// <summary>
+        /// Minimum output;
+        /// </summary>
+        public double MinOutput { get; set; } = double.MinValue;
+
+        /// <summary>
+        /// Maximum output.
+        /// </summary>
+        public double MaxOutput { get; set; } = double.MaxValue;
+
+        /// <summary>
+        /// Integral.
+        /// </summary>
+        public double Integral { get; private set; } = 0;
+
+
+        public double control(double desiredValue, double measuredValue, int durationMilliseconds)
+        {
+            double error = desiredValue - measuredValue;
+            Integral = Clamp(Integral + (Ki * error * durationMilliseconds));
+            double derivative = (error - PreviousError) / (double)durationMilliseconds;
+            double output = (Kp * error) + Integral + (Kd * derivative);
+            PreviousError = error;
+
+            output = Math.Max(MinOutput, output);
+            output = Math.Min(MaxOutput, output);
+
+            return output;
+        }
+
+        private double Clamp(double value)
+        {
+           double output = Math.Max(MinOutput, value);
+            output = Math.Min(MaxOutput, output);
+            return output;
+        }
+
+    }
+}
